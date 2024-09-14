38e1e0b09e5eb65a8eb6df3efd4a22a1c2f6d2bb
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-09-14 12:45:41 +0300

    final commit

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,30 @@
-import Home from "./pages/Home";
-import Movie from "./pages/Movie";
-import Directors from "./pages/Directors";
-import Actors from "./pages/Actors";
-import ErrorPage from "./pages/ErrorPage";
+import Home from './pages/Home'
+import Actors from './pages/Actors'
+import Directors from './pages/Directors'
+import Movie from './pages/Movie'
+import ErrorPage from './pages/ErrorPage'
 
 const routes = [
-    {
-      path: "/",
-      element: <Home />,
-      errorElement: <ErrorPage />
-    },
-    {
-      path: "/movie/:id",
-      element: <Movie />,
-      errorElement: <ErrorPage />
-    },
-    {
-      path: "/directors",
-      element: <Directors />,
-      errorElement: <ErrorPage />
-    },
-    {
-      path:"/actors",
-      element: <Actors />,
-      errorElement: <ErrorPage />
-    }
-  ]
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/actors',
+    element: <Actors />,
+  },
+  {
+    path: '/directors',
+    element: <Directors />,
+  },
+  {
+    path: '/movie/:id',
+    element: <Movie />,
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]
 
-export default routes;
+export default routes
